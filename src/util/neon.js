c0256a5720fb039a12c55957d8343b8f0288b7be
Yuri <snnp.ad.pnts@gmail.com>
2023-05-13 16:00:32 -0700

    Change env variable

--- src/util/neon.js
+++ src/util/neon.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg')
 const client = new Client({
-  connectionString: process.env.POSTGRES_CONNECTION_STRING,
+  connectionString: process.env.DATABASE_URL,
   ssl: true
 })
 
